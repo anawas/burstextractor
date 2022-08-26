5d13da6edf6bc617793acd47a036f02700ff5d41
Andreas Wassmer <andreas.wassmer@me.com>
2022-08-26 09:04:57 +0200

    Background correction according to Ariel

--- burstlist.py
+++ burstlist.py
@@ -5,6 +5,7 @@ author: Andreas Wassmer
 project: Raumschiff
 """
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from radiospectra.sources.callisto import CallistoSpectrogram
 from radiospectra import __version__
@@ -12,8 +13,9 @@ import timeutils
 import requests
 import datetime
 import logging
+import os
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.ERROR,
                     filename='app.log', filemode='w',
                     format='%(levelname)s - %(message)s')
 
@@ -59,15 +61,24 @@ def prettify(spectro):
     Applies some error corrections to the spectrogram. Those shall make the
     spectrogram look nicer.
     """
-    return spectro.elimwrongchannels().subtract_bg().denoise(full=True)
+    return spectro.elimwrongchannels().subtract_bg().denoise(full=False)
 
 
 def extract_burst(event):
     start, end = timeutils.extract_and_correct_time(event['Time'])
     start = start - datetime.timedelta(minutes=2)
+    if start.minute % 15 == 0:
+        start = start + datetime.timedelta(minutes=-1)
+
     end = end + datetime.timedelta(minutes=2)
+    if end.minute % 15 == 0:
+        end = end + datetime.timedelta(minutes=1)
 
     date = str(event['Date'])
+    path = f"bursts/{date}"
+    if not os.path.exists(path):
+        os.mkdir(path)
+
     event_start = f"{date[0:4]}/{date[4:6]}/{date[6:8]} {start.hour}:{start.minute}"
     event_end = f"{date[0:4]}/{date[4:6]}/{date[6:8]} {end.hour}:{end.minute}"
     instruments = event['Instruments'].split(',')
@@ -81,24 +92,40 @@ def extract_burst(event):
 
     for instr in instruments:
         if not instr.startswith('('):
-            logging.debug(f"Processing instrument {instr}")
+            logging.debug(f"Processing instrument {instr} for event from {event_start} to {event_end}")
             try:
                 s = CallistoSpectrogram.from_range(
                     instr, event_start, event_end)
-                s = s.subtract_bg()
-                # interesting = s.in_interval(event_start, event_end)
-                spec_mean = s.data.mean()
-                min = -5*spec_mean
-                max = 20*spec_mean
+                interesting = s.in_interval(event_start, event_end)
+                
+                # the last row in the masked array contains all nan, this we ignore it
+                pretty = prettify(interesting)
+                spec_mean = np.mean(pretty.data)
+                print(spec_mean)
+                
+                if np.abs(spec_mean) <= 0.0001:
+                    min = -5
+                    max = 10
+                else:
+                    min=-5*spec_mean
+                    max=20*spec_mean
 
+                # in rare occasions min > max
                 if min > max:
                     min *= -1
                     max *= -1
 
-                prettify(s).save(f"{instr}-{event['Date']}-{start.strftime('%H%M')}-{end.strftime('%H%M')}.fit.gz")
-                #s.peek(vmin=min, vmax=max)
+                fig = plt.figure(figsize=(6,4))
+                pretty.plot(fig, vmin=min, vmax=max, cmap=plt.get_cmap('plasma'))
+                fig.tight_layout()
+                
+                filename = f"{path}/{instr}_{event['Date']}_{start.strftime('%H%M')}_{end.strftime('%H%M')}"
+                plt.savefig(f"{filename}.jpg")
+                plt.close(fig)
+                pretty.save(f"{filename}.fit.gz")
 
             except Exception as e:
+                logging.error(f"While processing instrument {instr} for event from {event_start} to {event_end}")
                 logging.error("Exception occurred", exc_info=True)
                 continue
 
@@ -112,9 +139,10 @@ if __name__ == "__main__":
 
     print(f"\n Radiospectra version = {__version__}\n")
     filename = download_burst_list(2022, 8)
+    # filename = "e-CALLISTO_debug.txt"
     burst_list = process_burst_list(filename)
 
-    # Let's get all type IV bursts
+    # Let's get all type III bursts
     events = burst_list.loc[burst_list["Type"] == "III"]
     if len(events) > 0:
         print(f"Found {len(events)} event(s)")

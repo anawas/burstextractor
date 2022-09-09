01217a72f965c253fb6a25aefc1b07f8b7523334
Andreas Wassmer <andreas.wassmer@me.com>
2022-09-09 08:17:43 +0200

    Decoupling of functionality.

--- main.py
+++ main.py
@@ -10,19 +10,8 @@ import burstprocessor
 import datetime
 import logging
 
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                    filename='app.log', filemode='w',
-                    format='%(levelname)s - %(message)s')
-
-    logging.info(f"\n===== Start {datetime.datetime.now().strftime('%y-%m-%d %H:%M:%S')} =====\n")
-    print(f"\n Radiospectra version = {__version__}\n")
-    filename = burstlist.download_burst_list(2022, 9)
-    # filename = "e-CALLISTO_debug.txt"
-    burst_list = burstlist.process_burst_list(filename)
-
-    # Let's define all burst types that we want to process
+def extract_bursts(burst_list):
+        # Let's define all burst types that we want to process
     burst_types = ["I", "II", "III", "IV", "V"]
     for type in burst_types:
         events = burst_list.loc[burst_list["Type"] == type]
@@ -32,6 +21,27 @@ if __name__ == "__main__":
                 row = events.iloc[i]
                 burstprocessor.extract_burst(row)
         else:
-            print("No events found")
+            print(f"No events of type {type} found")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                    filename='app.log', filemode='w',
+                    format='%(levelname)s - %(message)s')
+
+    logging.info(f"\n===== Start {datetime.datetime.now().strftime('%y-%m-%d %H:%M:%S')} =====\n")
+    print(f"\n Radiospectra version = {__version__}\n")
+    for month in range (5,10):
+        # filename = burstlist.download_burst_list(2022, month)
+        if month < 10:
+            m = "0" + str(month)
+        else:
+            m = str(month)
+        
+        logging.info(f"\n----- Processing month {m} -----\n")
+        filename = f"e-CALLISTO_2022_{m}.txt"
+        burst_list = burstlist.process_burst_list(filename)
+        extract_bursts(burst_list)
     
     logging.info(f"\n===== End {datetime.datetime.now().strftime('%y-%m-%d %H:%M:%S')} =====\n")
+ 

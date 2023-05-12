4c74e64b04486525c7f13315a19eb175a487f4bb
Andreas Wassmer <andreas.wassmer@me.com>
2023-05-12 07:51:17 +0200

    Added day parameter to refelct new function call

--- tests/test_timeutils.py
+++ tests/test_timeutils.py
@@ -1,11 +1,12 @@
 import pytest
 from timeutils import extract_and_correct_time, adjust_year_month, check_valid_date
 
-"""
-Using fixtures here beacause I think that parametrizing
-the tests clutters the code in this case
-"""
+
 def test_extractandcorrecttime(event_times):
+    """
+    Using fixtures here beacause I think that parametrizing
+    the tests clutters the code in this case
+    """    
     for t in event_times:
         start, end = extract_and_correct_time(t)
         assert start.strftime("%H:%M"), end.strftime("%H:%M") == event_times[t]
@@ -14,7 +15,7 @@ def test_adjustyearmonth(event_dates):
     for d in event_dates:
         assert adjust_year_month(d[0], d[1]) == event_dates[d]
 
-@pytest.mark.parametrize("year, month", [(2022, 13), (2023, 1), (2022, "12")])
-def test_checkvaliddate(year, month):
+@pytest.mark.parametrize("year, month, day", [(2024, 3, 1), (2022, 13, 1), (2023, 1, 41), (2022, "12", 1)])
+def test_checkvaliddate(year, month, day):
     with pytest.raises(AssertionError):
-        check_valid_date(year, month)
+        check_valid_date(year, month, day)
